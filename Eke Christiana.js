177bf026f8ba4be4d5780d3be3229aae000e3132
Crixxee <105209469+Crixxee@users.noreply.github.com>
2022-06-24 01:24:20 +0100

    Uploaded the file

---
+++ Eke Christiana.js
@@ -0,0 +1,5 @@
+// Write a javascript code that prints out your name, height, and country on the screen task
+
+console.log('Name: Eke Adaobi Christiana')
+console.log('Height: 178cm');
+console.log('Country: Nigeria');
